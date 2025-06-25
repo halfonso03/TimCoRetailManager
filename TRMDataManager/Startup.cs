3d62da951afdd638dd6e6ae3e57c89d339feb865
Hector Alfonso <hector.alfonso@yahoo.com>
2025-06-25 10:47:49 -0400

    Add Swagger Documentation ti API
    Add swashbuckle/swagger tot he api for documentation and testing

--- TRMDataManager/Startup.cs
+++ TRMDataManager/Startup.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using Microsoft.Owin;
+using Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Owin;
-using Owin;
+using System.Web.Http;
 
 [assembly: OwinStartup(typeof(TRMDataManager.Startup))]
 
