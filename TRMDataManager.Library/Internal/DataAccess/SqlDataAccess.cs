ab70bf94ae15b5e70639a23f08288fe7552fd08c
Hector Alfonso <hector.alfonso@yahoo.com>
2025-06-26 14:59:22 -0400

    Add sql Transactions

---
+++ TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,106 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDataManager.Library.Internal.DataAccess
+{
+    using Dapper;
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace TRMDataManager.Library.Internal.DataAccess
+    {
+        internal class SqlDataAccess : IDisposable
+        {
+            public string GetConnectionString(string connectionString)
+            {
+                return ConfigurationManager.ConnectionStrings[connectionString].ConnectionString;
+            }
+
+            public List<T> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+            {
+                string connectionString = GetConnectionString(connectionStringName);
+
+                using (IDbConnection conn = new SqlConnection(connectionString))
+                {
+                    List<T> rows = conn.Query<T>(storedProcedure, parameters,
+                        commandType: CommandType.StoredProcedure).ToList();
+
+                    return rows;
+                }
+            }
+
+            public void SaveData<T>(string storedProcedure, T parameters, string connectionStringName)
+            {
+                string connectionString = GetConnectionString(connectionStringName);
+
+                using (IDbConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Execute(storedProcedure, parameters,
+                        commandType: CommandType.StoredProcedure);
+
+                }
+            }
+
+            //Open connection/start transaction method
+            //load using transaction
+            //save using transaction
+            //close connection/stop transaction method
+            //Dispose
+
+            private IDbConnection _connection;
+            private IDbTransaction _transaction;
+
+            public void StartTransaction(string connectionStringName)
+            {
+                string connectionString = GetConnectionString(connectionStringName);
+                _connection = new SqlConnection(connectionString);
+                _connection.Open();
+
+                _transaction = _connection.BeginTransaction();
+            }
+            public List<T> LoadDataInTransaction<T, U>(string storedProcedure, U parameters)
+            {
+                List<T> rows = _connection.Query<T>(storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure, transaction: _transaction).ToList();
+
+                return rows;
+            }
+
+            public void SaveDataInTransaction<T>(string storedProcedure, T parameters)
+            {
+                _connection.Execute(storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure, transaction: _transaction);
+            }
+
+            public void CommitTransaction()
+            {
+                _transaction?.Commit();
+                _connection?.Close();
+            }
+
+            public void RollbackTransaction()
+            {
+                _transaction?.Rollback();
+                _connection?.Close();
+            }
+
+            public void Dispose()
+            {
+                CommitTransaction();
+            }
+        }
+    }
+}
