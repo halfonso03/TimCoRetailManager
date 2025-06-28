c7105880b5faa4c8d0444f67481f219d7aeda52d
Hector <hector.alfonso@yahoo.com>
2025-06-28 10:56:52 -0400

    Add FK Relationships

--- TRMData/dbo/Tables/SaleTable.sql
+++ TRMData/dbo/Tables/SaleTable.sql
@@ -5,5 +5,6 @@
     [SaleDate] DATETIME2 NOT NULL, 
     [SubTotal] MONEY NOT NULL, 
     [Tax] MONEY NOT NULL, 
-    [Total] MONEY NOT NULL
+    [Total] MONEY NOT NULL, 
+    CONSTRAINT [FK_SaleTable_ToUser] FOREIGN KEY ([CashierId]) REFERENCES [Users]([Id])
 )
